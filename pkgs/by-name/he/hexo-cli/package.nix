54d166d7af35ff8eb4e61e172dcb115d103680a1
phanirithvij <phanirithvij2000@gmail.com>
2025-10-16 12:05:42 +0530

    WIP hexo-cli: 4.3.1 -> 4.3.2
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- pkgs/by-name/he/hexo-cli/package.nix
+++ pkgs/by-name/he/hexo-cli/package.nix
@@ -4,32 +4,41 @@
   fetchFromGitHub,
 }:
 
-buildNpmPackage rec {
+buildNpmPackage (finalAttrs: {
   pname = "hexo-cli";
-  version = "4.3.1";
+  version = "4.3.2";
 
   src = fetchFromGitHub {
-    owner = "hexojs";
+    owner = "phanirithvij";
     repo = "hexo-cli";
-    rev = "v${version}";
-    hash = "sha256-mtbg9Fa9LBqG/aNfm4yEo4ymuaxuqhymkO1q6mYA2Fs=";
+    rev = "refs/heads/add-package-lock";
+    hash = "sha256-e6wy7/TpGlwXxqSHfSbbuZi5TmoHP9pWZESy2RzR/6Q=";
   };
 
-  npmDepsHash = "sha256-VCHG1YMPRwEBbwgscSv6V+fTNVRpsCxWeyO8co4Zy6k=";
+  npmDepsHash = "sha256-ypijEo2BFvA1lzw6S6FvtRay2gERK0iKVvuWd5rQfUI=";
 
   installPhase = ''
     runHook preInstall
-
     mkdir -p $out
     cp -r bin/ dist/ node_modules/ package.json $out/
-
     runHook postInstall
   '';
 
+  doInstallCheck = true;
+  # versionCheckHook succeeds with a error message, hence a custom install check
+  installCheckPhase = ''
+    runHook preInstallCheck
+    ($out/bin/hexo version 2>/dev/null || true) | grep -F "hexo-cli: ${finalAttrs.version}" \
+      || (echo "ERROR: version check failed. expected 'hexo-cli: ${finalAttrs.version}'" && exit 1)
+    runHook postInstallCheck
+  '';
+
   meta = {
+    changelog = "https://github.com/hexojs/hexo-cli/releases/tag/${finalAttrs.src.tag}";
     description = "Command line interface for Hexo";
     mainProgram = "hexo";
     homepage = "https://hexo.io/";
     license = lib.licenses.mit;
+    maintainers = [ ];
   };
-}
+})
