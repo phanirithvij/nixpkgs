0b4e334ff36821d3d550268b89e700e6c4ed2a58
phanirithvij <phanirithvij2000@gmail.com>
2025-10-19 23:52:06 +0530

    [WIP] anastasis: init at 0.7.0
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

---
+++ pkgs/by-name/an/anastasis/package.nix
@@ -0,0 +1,119 @@
+{
+  stdenv,
+  fetchgit,
+  pkg-config,
+  autoreconfHook,
+  taler-exchange,
+  taler-merchant,
+  libgcrypt,
+  libmicrohttpd,
+  jansson,
+  libsodium,
+  postgresql,
+  curl,
+  recutils,
+  libuuid,
+  lib,
+  gnunet,
+  makeWrapper,
+  jq,
+  texinfo,
+  which,
+  callPackage,
+  gitUpdater,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "anastasis";
+  #version = "0.6.6";
+  version = "0.6.6-unstable-2025-10-08"; # 0.7.0
+
+  src = fetchgit {
+    url = "https://git.taler.net/anastasis.git";
+    #tag = "v${finalAttrs.version}";
+    #hash = "sha256-TVopOhVMGNWE2WD1swikzpcNP2DSbJhtQHBBGX/a7vA="; # 0.6.6
+    rev = "925fc7d5e57752607d1bf76d367979c1f5e6b3fa";
+    hash = "sha256-Vl0OE/6PGVEyQ9ZlkoD4bPspW5/Y0skrz+ng0P2xlRk="; # 0.7.0
+  };
+
+  postPatch = ''
+    patchShebangs contrib src/cli
+    ./contrib/gana-update.sh
+  '';
+
+  outputs = [
+    "out"
+    "configured"
+  ];
+
+  nativeBuildInputs = [
+    pkg-config # hook that adds pkg-config files of buildInputs
+    autoreconfHook # hook that triggers autoreconf to get the configure script
+    makeWrapper # for wrapProgram
+    jq
+    texinfo
+  ];
+
+  buildInputs = [
+    taler-exchange
+    taler-merchant
+    libgcrypt
+    libmicrohttpd
+    libsodium
+    postgresql
+    curl
+    jansson
+    recutils
+    libuuid
+  ];
+
+  configureFlags = [
+    "--with-gnunet=${gnunet}"
+    "--with-exchange=${taler-exchange}"
+    "--with-merchant=${taler-merchant}"
+  ];
+
+  postConfigure = ''
+    mkdir -p $configured
+    cp -r ./* $configured/
+  '';
+
+  postInstall = ''
+    wrapProgram $out/bin/anastasis-config --prefix PATH : ${
+      lib.makeBinPath [
+        # Fix "anastasis-config-wrapped needs gnunet-config to be installed"
+        #   in src/cli/test_anastasis_reducer_backup_enter_user_attributes.sh
+        # (NB: --with-gnunet was not enough)
+        gnunet
+        # needed by $out/bin/anastasis-config during postInstallCheck
+        which
+      ]
+    }
+  '';
+
+  doInstallCheck = true;
+
+  # Check that anastasis-config can find gnunet at runtime
+  installCheckPhase = ''
+    runHook preInstallCheck
+
+    output=$($out/bin/anastasis-config --help || true)
+    echo "$output" | grep "Report bugs to contact@anastasis.lu."
+
+    runHook postInstallCheck
+  '';
+
+  passthru.tests.vmTest = callPackage ./test.nix { };
+  passthru.updateScript = gitUpdater { rev-prefix = "v"; };
+
+  meta = {
+    description = ''
+      GNU Anastasis is a key backup and recovery tool from the GNU project.
+      This package includes the backend run by the Anastasis providers as
+      well as libraries for clients and a command-line interface.
+    '';
+    license = lib.licenses.agpl3Plus; # from the README
+    homepage = "https://anastasis.lu";
+    maintainers = with lib.maintainers; [ ];
+    teams = with lib.teams; [ ngi ];
+  };
+})
