e07b237b07ec9e09b5b84425f3f7a83c0cb3c05a
phanirithvij <phanirithvij2000@gmail.com>
2026-01-20 14:39:37 +0530

    WIP fixup
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- pkgs/applications/office/libreoffice/src-collabora/main.nix
+++ pkgs/applications/office/libreoffice/src-collabora/main.nix
@@ -1,7 +1,7 @@
 { fetchgit, ... }:
 fetchgit {
   url = "https://gerrit.libreoffice.org/core";
-  rev = "refs/tags/cp-25.04.8-1";
+  rev = "cp-25.04.8-1";
   hash = "sha256-Ddof55XENNbxsho1NT+O5Qrz0bTQurx42EPICRLvfPs=";
   fetchSubmodules = false;
 }
--- pkgs/applications/office/libreoffice/src-collabora/translations.nix
+++ pkgs/applications/office/libreoffice/src-collabora/translations.nix
@@ -1,6 +1,6 @@
 { fetchgit, ... }:
 fetchgit {
   url = "https://gerrit.libreoffice.org/translations";
-  rev = "refs/tags/cp-25.04.8-1";
+  rev = "cp-25.04.8-1";
   hash = "sha256-oV9SqwsFFWvDL1J+BjOd2YPNvmwsNwVeuib2sKIKXwU=";
 }
