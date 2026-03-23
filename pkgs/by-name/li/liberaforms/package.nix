37a49a8787fc5438492e3e72cdd30a500b18dd98
phanirithvij <phanirithvij2000@gmail.com>
2026-03-23 23:21:11 +0530

    WIP
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- pkgs/by-name/li/liberaforms/package.nix
+++ pkgs/by-name/li/liberaforms/package.nix
@@ -26,6 +26,40 @@ let
         };
         doCheck = false;
       };
+      # required when downgrading flask-migrate to 3.1.0
+      flask_2_2_2 = python3Packages.flask.overridePythonAttrs rec {
+        version = "2.2.2";
+        pyproject = null;
+        format = "setuptools";
+        src = fetchPypi {
+          pname = "Flask";
+          inherit version;
+          hash = "sha256-ZCxFDRnErUgvlnKb0qj20yVUqh4jH09rTn5SZLFsyis=";
+        };
+        dependencies = (
+          with python3Packages;
+          [
+            click
+            blinker
+            itsdangerous
+            jinja2
+          ]
+          ++ [ self.werkzeug_2_2_2 ]
+        );
+        nativeCheckInputs = [ ];
+      };
+      # required when downgrading flask-migrate to 3.1.0
+      werkzeug_2_2_2 = super.werkzeug.overridePythonAttrs rec {
+        version = "2.2.2";
+        pyproject = null;
+        format = "setuptools";
+        src = fetchPypi {
+          pname = "Werkzeug";
+          inherit version;
+          hash = "sha256-fqLUgyLMfA+LOiFe1z6r17XXXQtQ4xqwBihsz/ngC48=";
+        };
+        nativeCheckInputs = [ ];
+      };
       # required 3.1.0 (requirements and 3.1.0 works with alembic 1.8.1)
       flask-migrate = super.flask-migrate.overridePythonAttrs (old: rec {
         version = "3.1.0";
@@ -35,7 +69,12 @@ let
           tag = "v${version}";
           hash = "sha256-2P9UfR/1Vv9FSmpSZn0gV4/uuSNdl6sdgRnSbefFR34=";
         };
-        dependencies = (old.dependencies or [ ]) ++ [ self.alembic ];
+        dependencies = [
+          self.alembic
+          self.flask_2_2_2
+          self.flask-sqlalchemy
+        ];
+        nativeCheckInputs = [ ];
       });
       # required downgrade to 3.0.2 (from requirements.txt as well as sqlalchemy downgrade)
       flask-sqlalchemy = super.flask-sqlalchemy.overridePythonAttrs (old: rec {
@@ -45,7 +84,11 @@ let
           inherit version;
           hash = "sha256-FhmfWz3ftp4N8vUq5Mdq7b/sgjRiNJ2rshobLgorZek=";
         };
-        dependencies = (old.dependencies or [ ]) ++ [ super.pdm-pep517 ];
+        dependencies = [
+          self.flask_2_2_2
+          super.sqlalchemy
+          super.pdm-pep517
+        ];
       });
       # required 1.4.42 (from requirements)
       sqlalchemy = super.sqlalchemy_1_4.overridePythonAttrs rec {
