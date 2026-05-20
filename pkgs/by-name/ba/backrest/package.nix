7cbc3cf44773a8d5398087635cdbfd1b793c70c2
phanirithvij <phanirithvij2000@gmail.com>
2026-05-20 23:28:26 +0530

    backrest.services.default: init
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- pkgs/by-name/ba/backrest/package.nix
+++ pkgs/by-name/ba/backrest/package.nix
@@ -181,6 +181,10 @@ buildGoModule (finalAttrs: {
       })
       ./update-inlang-plugins.sh
     ];
+    services.default = {
+      imports = [ (lib.modules.importApply ./service.nix { }) ];
+      backrest.package = finalAttrs.finalPackage;
+    };
   };
 
   meta = {
