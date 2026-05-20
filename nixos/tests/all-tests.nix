65c879799883b72617f9cd4db794cc1375c138f4
phanirithvij <phanirithvij2000@gmail.com>
2026-05-20 23:28:26 +0530

    backrest.services.default: init
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -1468,6 +1468,7 @@ in
   peertube = handleTestOn [ "x86_64-linux" ] ./web-apps/peertube.nix { };
   perses = runTest ./perses.nix;
   pgadmin4 = runTest ./pgadmin4.nix;
+  backrest-modular = runTest ./backrest-modular.nix;
   pgbackrest = import ./pgbackrest { inherit runTest; };
   pgbouncer = runTest ./pgbouncer.nix;
   pgmanage = runTest ./pgmanage.nix;
